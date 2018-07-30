4844c51232afe23364faeea32804959b87b3d1f1
farscapian <derek.l.smith@protonmail.com>
2018-07-30 12:52:39 -0700

    Deleted deploy_stack.sh

--- multipass/lxd/bcm/bitcoin/deploy_stack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# # change permissions and execute /entrypoint.sh
-lxc exec manager1 -- chmod +x /apps/bitcoin/bitcoinstack/up.sh
-lxc exec manager1 -- bash -c /apps/bitcoin/bitcoinstack/up.sh
