725eec28f885ebbc88a92870c5598820666c4e21
Derek Smith <derek@farscapian.com>
2018-10-26 14:59:49 +0000

    Added trezor integration to dev_machine.

--- dev_machine/trezor/commit_sign_git_repo.sh
+++ dev_machine/trezor/commit_sign_git_repo.sh
@@ -21,10 +21,13 @@ git commit -a -m "$BCM_GIT_COMMIT_MESSAGE" --gpg-sign
 if [[ $BCM_GIT_AUTO_PUSH = "true" ]]; then
     echo "We're going to push the repo over TOR."
 
+    echo "Starting tor in the background."
+    tor &
+    wait-for-it -t 0 127.0.0.1:9050
+
     git config --global http.proxy socks5://127.0.0.1:9050
     echo "git config --global http.proxy:  $(git config --global --get http.proxy)"
 
-    wait-for-it -t 0 127.0.0.1:9050
 
     git push
 echo
