2a6970676f056b9fe0b331e5fccae0c6d0994188
derek <derek@upstairs>
2018-12-03 15:25:07 +0000

    Refined setup.sh

--- setup.sh
+++ setup.sh
@@ -15,39 +15,37 @@ if [[ -z $(git config --get --local user.email) ]]; then
 fi
 
 # let's make sure the local git client is using TOR for git push/pull operations for the BCM github URL only.
-BCM_GITHUB_URL="https://github.com/BitcoinCacheMachine/BitcoinCacheMachine"
-BCM_LOCAL_REPO_HTTP_PROXY="socks5://localhost:9050"
-
-if [[ $(git config --get --global http.$BCM_GITHUB_URL.proxy) != "$BCM_LOCAL_REPO_HTTP_PROXY" ]]; then
-  echo "Setting git client to use local TOR proxy for URL '$BCM_GITHUB_URL'"
-  git config --global "http.$BCM_GITHUB_URL.proxy" $BCM_LOCAL_REPO_HTTP_PROXY
+BCM_TOR_PROXY="socks5://localhost:9050"
+if [[ $(git config --get --local http.proxy) != "$BCM_TOR_PROXY" ]]; then
+  echo "Setting git client to use local SOCKS5 TOR proxy for push/pull operations."
+  git config --local http.proxy "$BCM_TOR_PROXY"
 fi
 
 # get the current directory where this script is so we can reference it later
 echo "Setting BCM_GIT_DIR environment variable in current shell to '$(pwd)'"
 BCM_GIT_DIR=$(pwd)
-export BCM_GIT_DIR
+export BCM_GIT_DIR="$BCM_GIT_DIR"
 export BCM_RUNTIME_DIR="$HOME/.bcm"
 
 BCM_BASHRC_START_FLAG='###START_BCM###'
 BCM_BASHRC_END_FLAG='###END_BCM###'
-PROFILE_FILE="$HOME/.bashrc"
-if grep -Fxq "$BCM_BASHRC_START_FLAG" "$PROFILE_FILE"
-then
-  # code if found
-  echo "BCM flag discovered in '$PROFILE_FILE'. Please inspect your '$PROFILE_FILE' to clear any BCM-related content, if appropriate."
-else
-  echo "Writing commands to '$PROFILE_FILE' to support running BCM from the admin machine."
+BASHRC_FILE="$HOME/.bashrc"
 
+if grep -Fxq "$BCM_BASHRC_START_FLAG" "$BASHRC_FILE"; then
+  # code if found
+  echo "BCM flag discovered in '$BASHRC_FILE'. Please inspect your '$BASHRC_FILE' to clear any BCM-related content, if appropriate."
+  exit
+else
+  echo "Writing commands to '$BASHRC_FILE' to support running BCM from the admin machine."
   {
     echo "$BCM_BASHRC_START_FLAG" 
     echo "export BCM_GIT_DIR=$BCM_GIT_DIR"
+    echo "export BCM_RUNTIME_DIR=$BCM_RUNTIME_DIR"
     
     # shellcheck disable=SC2016
     echo "export PATH="'$PATH:'""'$BCM_GIT_DIR/cli'""
-    echo "export BCM_RUNTIME_DIR=$BCM_RUNTIME_DIR"
     echo "$BCM_BASHRC_END_FLAG"
-  } >> "$PROFILE_FILE"
+  } >> "$BASHRC_FILE"
 fi
 
 echo "Done setting up your machine to use the Bitcoin Cache Machine CLI. Type 'bcm' to continue."
