fd2c56cdeff5e12105520ca1d53ef6f0f03b288b
farscapian <derek@farscapian.com>
2019-09-09 22:18:58 +0000

    Improved remote cluster deployment via 'bcm ssh prepare'.

--- commands/cluster/server_prep.sh
+++ commands/cluster/server_prep.sh
@@ -66,8 +66,8 @@ if [[ ! -f "$HOME/.ssh/authorized_keys" ]]; then
 fi
 
 # TODO verify where we need this.
-sudo touch /etc/sudoers.d/bcm
-echo "bcm ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/bcm
+sudo touch /etc/sudoers.d/$USERNAME
+echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee -a "/etc/sudoers.d/$USERNAME"
 
 # update /etc/ssh/sshd_config to listen for incoming SSH connections on all interfaces.
 if ! grep -Fxq "ListenAddress 0.0.0.0" /etc/ssh/sshd_config; then
