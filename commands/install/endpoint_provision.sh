84fa923d335bff33ebc848325d05957d6b22208f
farscapian <derek@farscapian.com>
2019-09-11 18:44:57 +0000

    Improved remote cluster deployment via 'bcm ssh prepare'.

--- commands/install/endpoint_provision.sh
+++ commands/install/endpoint_provision.sh
@@ -28,6 +28,7 @@ if [ ! -x "$(command -v lxd)" ]; then
     # unless this is modified, we get snapshot creation in snap when removing lxd.
     echo "Info: installing 'lxd' on $HOSTNAME."
     sudo snap install lxd --channel="$BCM_LXD_SNAP_CHANNEL"
+    sleep 5
     sudo snap set system snapshots.automatic.retention=no
 fi
 
