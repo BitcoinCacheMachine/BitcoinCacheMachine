a32a78ebe31d9cce1c58383bcae1ce22654f0f33
farscapian <derek@farscapian.com>
2019-09-11 23:30:27 +0000

    Improved remote cluster deployment via 'bcm ssh prepare'.

--- controller/install_docker_client.sh
+++ controller/install_docker_client.sh
@@ -13,22 +13,22 @@ fi
 
 if ! snap list | grep -q docker; then
     echo "Info: installing 'docker' locally."
-    sudo snap install docker --channel=candidate
+    sudo snap install docker --channel="stable"
+    
+    # next we need to determine the underlying file system so we can upload the correct daemon.json
+    DEVICE="$(df -h "$HOME" | grep ' /' | awk '{print $1}')"
+    FILESYSTEM="$(mount | grep "$DEVICE")"
+    
+    DAEMON_CONFIG="$BCM_GIT_DIR/commands/install/overlay_daemon.json"
+    if echo "$FILESYSTEM" | grep -q "btrfs"; then
+        DAEMON_CONFIG="$BCM_GIT_DIR/commands/install/btrfs_daemon.json"
+    fi
+    
+    DAEMON_FILE="/var/snap/docker/current/config/daemon.json"
+    echo "INFO: Setting dockerd daemon settings to $DAEMON_FILE"
+    sudo cp "$DAEMON_CONFIG" "$DAEMON_FILE"
+    
+    sudo snap restart docker
+    sleep 5
 fi
 
-# next we need to determine the underlying file system so we can upload the correct daemon.json
-DEVICE="$(df -h "$HOME" | grep ' /' | awk '{print $1}')"
-FILESYSTEM="$(mount | grep "$DEVICE")"
-
-
-DAEMON_CONFIG="$BCM_GIT_DIR/commands/install/overlay_daemon.json"
-if echo "$FILESYSTEM" | grep -q "btrfs"; then
-    DAEMON_CONFIG="$BCM_GIT_DIR/commands/install/btrfs_daemon.json"
-fi
-
-DAEMON_FILE="/var/snap/docker/current/config/daemon.json"
-echo "INFO: Setting dockerd daemon settings to $DAEMON_FILE"
-sudo cp "$DAEMON_CONFIG" "$DAEMON_FILE"
-
-sudo snap restart docker
-sleep 5
