5f6fb15fa502a244b2b9c12466466877b34a39d9
farscapian <derek@farscapian>
2020-04-11 12:19:23 -0500

    automated commit push.

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 set -Eeuox pipefail
 cd "$(dirname "$0")"
 
-exit 1
-
 # remove any pre-existing software that may exist and have conflicts.
 for PKG in lxd lxd-client tor; do
     if dpkg -s "$PKG" >/dev/null 2>&1; then
