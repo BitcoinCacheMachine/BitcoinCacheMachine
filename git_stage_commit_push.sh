a621634c24932af2f7d4e0bdccf2cfb7d9d64983
farscapian <derek@farscapian.com>
2020-05-13 13:23:49 -0400

    automated commit push.

--- git_stage_commit_push.sh
+++ git_stage_commit_push.sh
@@ -10,5 +10,5 @@ source ./env
 # pull git from the published location. Note we do NOT mount BCM_GIT_DIR
 # from the controller; it's always pulled from the GIT server endpoint
 git add *
-git commit --message="automated commit push."
+git commit -S --message="automated commit push."
 git push
